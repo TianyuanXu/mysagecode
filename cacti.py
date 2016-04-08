de3d04aca902a465b0a6b56d4956d521c14ea6a1
TianyuanXu <tianyuan@uoregon.edu>
2016-04-08 10:38:40 -0700

    incorporate calculations for dihedral groups

--- cacti.py
+++ cacti.py
@@ -1,7 +1,7 @@
 # To be used only when hecke.py has been loaded.
+import hecke
 
-
-# Digression: computatzion of T_w0 * c_w.
+# Computatzion of T_w0 * c_w.
 def ts_times_cw(type,s,w):
     """ Compute T_s*c_w in the Hecke algebra.
 
@@ -121,5 +121,70 @@ def convert_expression_BDFH(rank,w):
 
 def convert_lcells_BDFH(rank,d):
     return {k:[convert_expression_BDFH(rank,w) for w in d[k]] for k in d}
+
+"""
+Code for Dihedral Groups
+"""
+
+
+############################ Dihedral Case ####################################
+
+def dihedral_ts_times_cw(rank,s,w):
+    """ 
+    Compute $T_s*c_w$ in the dihedral group(with symbols 1,2) with given rank.
+    """
+
+    d = defaultdict()
+    if len(w) == rank:
+        d[dihedral_string(1,2,rank)] = v
+    elif s == w[0]:
+        d[w] = v
+    else:
+        d[(s,)+w]=1
+        d[w]=-v**(-1)
+        if len(w) > 1: 
+            d[w[1:]]=1
+    return d
+
+def dihedral_tproduct_times_cw(rank,t,w):
+    d = defaultdict()
+
+    d[w] = 1
+    for s in reversed(t): 
+        ts_times_d = defaultdict(int)
+        for x in d:
+            dd = dihedral_ts_times_cw(rank,s,x)
+            for term in dd:
+                ts_times_d[term] += dd[term] * d[x]
+        d = remove_zero(ts_times_d)  # see below for definition of remove_zero
+    return d
+
+def dihedral_tw0_times_cw(rank,w):
+    w0=dihedral_string(1,2,rank)
+    d = dihedral_tproduct_times_cw(rank,w0,w)
+    return compress_key(d)
+
+def dihedral_inv(rank,l,w):
+    d = dihedral_tw0_times_cw(rank,w)
+    for x in l:
+        if x in d:
+            return x
+        else: 
+            continue
+
+def dihedral_print_inv(rank):
+    print 'The involution sigma behaves as follows in the dihedral group I_2({}):'.format(rank)
+    print ''
+    i = 1
+    d = dihedral_lcells(rank)
+    for k in d:
+        print 'Left cell #{}: {}'.format(i,d[k])
+        for elt in d[k]:
+            print '{} --> {}'.format(elt,dihedral_inv(rank,d[k],tuple(int(i)
+                for i in str(elt)))) 
+        print ''
+        i = i+1
+
+
 
 
