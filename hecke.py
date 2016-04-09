468aae7b6a91a671ddbf04ee4876563b1fbd0fc6
Tianyuan Xu <tianyuan@uoregon.edu>
2016-04-08 23:17:46 -0600

    update hecke.py

--- hecke.py
+++ hecke.py
@@ -1,156 +1,136 @@
 import itertools
 from collections import defaultdict
 
+r"""
+
+This file contains code for fast computation of products of Kazhdan-Lusztig
+basis elements in Hecke algebras.
+
+-- Key Example
+
+We illustrate our algorithm using a very simple example. 
+(Please see the next section for more remarks on notations.)
+In the Hecke algebra of type ['A',4] (i.e., that of the symmetric group 'S_5'), 
+we have
+
+\[
+c_{121}c_{121} = (c_{1}c_{21} - c_1)c_{121} = (c_1c_2c_1 - c_1) c_121
+\]
+
+In this example, we expressed the left factor 'c_{121}' as a sum of products of
+
+
+
+-- Notations and Conventions
+
+We use the notations and conventions used by Lusztig in his book "Hecke
+algebras with unequal parameters".  
+
+- Coxeter groups are given by types(e.g., ['A',4]) and usually denoted by 'W'.
+- Simple reflections are usually denoted by 's' or 't'.
+- Elements of Coxeter groups are denoted by lower letters like the 'w' or 'y'.
+- Elements of Coxeter groups are implemented as tuples, e.g., 's_1s_2=(1,2)'.
+- For 'w\in W', 'l(w)' denotes the length of 'w'.
+- The Bruhat order is denoted by '<'. 
+- Standard basis elements of Hecke algebras are denoted by 'T_w'.
+- Kazhdan-Lusztig basis elements of Hecke algebras are denoted by 'c_w'.
+- 'kl' always stands for 'Kazhdan-Lusztig'.
+- We use the normalization '(T_s-v)(T_s+v^{-1})=0' for the quadratic relation
+  in the definition of the Hecke algebra. 
+- Kazhdan-Lusztig polynomials are denoted by 'p_{y,w}'.
+- We will use the following fact: 
+     'c_s \times c_w = (v+v^{-1}) c_w' if 'sw<w';
+     'c_s \times c_w = c_{sw} + \sum_{y: sy<y<w} \mu_{y,w} c_y' if 'sw>w'.
+- We call the nonnegative integer coefficient '\mu_{y,w}' a mu-coefficient.
+- Computation of mu-coefficients is at the center of our algorithm. We will
+  compute them by using the package Coxeter3(wrapped in Sage), which seems to
+  be the fastest way.  
+- In the future we may also need to compute Kazhdan-Lusztig polynomials using
+  Coxeter3. Coxeter3 uses a normalization for Hecke algebras that is different
+  from ours, so a conversion would be necessary(see the function kl(type,y,w)).
 
 """
-Background:
-    There are two sets of conventions for Hecke algebra.
-    Ours: 
-    (H_s-v)(H_s+v^{-1})=0
-    Theirs (the one used by Sage and Coxeter3):
-    (T_s-q)(T_s+1)=0, 
-    implying (T_s/q^{1/2}-q^{1/2})(T_s/q^{1/2}+q^{-1/2})=0
 
-    So there is an algebra homomorphism \phi sending our H_s to their
-    T_s/q^{1/2}, and sending our v to their q^{1/2}. Meanwhile, on each version
-    of the Hecke algebras there is a "bar involution" of algebras, with ours 
-    sending H_s to its inverse and v to v^{-1} and theirs sending T_s to its 
-    inverse and sending q^{1/2} to q^{-1/2}. Thus, the bar involution simply 
-    sends each term to its inverse in the Hecke algebra. Being an algebra
-    homomorphism, \phi commutes with the bar involution. So the \phi-image of 
-    our Kazhdan-Lusztig basis elements are their Kazhdan-Lusztig basis elements 
-    (for Sage, the images are denoted by Cp, with C used for another basis),
-    which are defined to be special bar-invariant elements in each case.
-
-    Conversion between the two versions of Kazhdan-Lusztig(KL) polynomials:
-    For us, the KL polynomials are characterized as follows:
-    c_w=\sum_{y\le w} p_{y,w}(v)H_y;
-    For Sage and Coxeter3, the KL polynomials are characterized as follows:
-    c_w=q^{-l(w)/2}\sum_{y\le w} p_{y,w}(q)T_y.
-    (Again, in Sage, this holds with Cp in place of C.)
-    Using the fact that \phi(c_w)=c_w, it is then straightforward to figure out 
-    how to convert the two versions of KL polynomials.
     
-    From ours to theirs: 
-    Mulitply p_{y,w}(v) by v^{l(w)-l(y)}, then replace each v with q^{1/2}. 
-    This results in p_{y,w}(q).
+def v_times_w(type,v,w):
+    r""" Compute c_v*c_w in the Hecke algebra.
 
-    From theirs to ours:
-    Multiply p_{y,w}(q) by q^{(l(y)-l(w))/2}, then replace each q with v^2.
-"""
+    INPUT: 
+ 
+    - 'type' -- the Cartan type of a Coxeter group 'W'
+    - 'v,w' -- tuples, representing elements of 'W'
 
-
-""" Things I'm not using at the moment
-
-
-# variable setup
-R.<q,v>=LaurentPolynomialRing(QQ,2)
-
-
-# Computations using Sage's internal algorithms
-# A Coxeter group can be defined using its Cartan type, or its Coxeter matrix.
-
-m=[[1,4,3,4],[4,1,3,2],[3,3,1,2],[4,2,2,1]] 
-W1=CoxeterGroup(m)
-
-
-# Immediately declare s=W.simple_reflections() after defining W; 
-# Indexing of s starts at 1. 
-# Or declare something like [s1,s2,s3]=W.simple_reflections().
-
-
-
-# Computations using Gap3-Cheive:
-# attach pycox.py and define Coxeter groups using Cartan type or Cartan matrix.
-# Computattion is faster than Sage but mostly only deals with finite groups.
-
-"""
-
-# Computation using coxeter3, for faster computation of KL polynomials:
-
-var('v')
-# var('q')
-
-def coxeter3(l,rank):
-    return CoxeterGroup([l,rank],implementation='coxeter3')
-
-def kl(type,y,w):
-    """ Compute a Kazhdan-Lusztig polynomial p_{y,w}.
-
-    INPUT:
-    - "type" -- the Cartan type of a Coxeter group W
-    - "y", "w" --tuples, considered as expressions(not necessarily reduced) for
-                 elements in W
-   
     OUTPUT:
-    - the Kazhdan-Lusztig polynomial p_{y,w}, returned in our convention. 
+    - the product 'c_v * c_w' in the Hecke algebra
 
+    EXAMPLES:
 
-    .. TODO::
-    - use Coxeter matrix instead of Cartan type to specify Coxeter groups.
-    """
-   
-    W = CoxeterGroup(type,implementation='coxeter3')
-    f = W.kazhdan_lusztig_polynomial(y,w)
-    g = f.substitute(q=v**2)*v**(len(y)-len(w))
-    return g.expand()
+        sage: v_times_w(['A',4],(2,1,3,2,1,4),(2,1))
+        sage: {(2,1,3,2,1,4): (v + 1/v)^2}
 
-def mu(type,y,w):
-    """ Compute the mu_{y,w}, the mu-coefficient for a pair of elements y,w.
-    
-    .. TODO::
-        Use the coxeter3 package to directly compute mu_{y,w} instead of 
-        reading it off as a coefficient of p_{y,w}.
-        The direct computation can be---and is---done in s_times_w, so this
-        function is actually not used.
+        sage: v_times_w(['A',4],(1,2,1), (1,2,1))
+        sage: {(1,2,1): (v + 1/v)^3 - v - 1/v} 
     """
 
-    return kl(type,y,w).coefficient(v,-1)
-    
+    d = break_elt(type,v)
+    return sproducts_times_w(type,d,w)
+
+
+
 def s_times_w(type,s,w):
-    """ Compute the product c_s*c_w, ere s is a simple reflection.
+    r""" 
+    Compute the product 'c_s*c_w', where 's' is a simple reflection.
     
     INPUT:
-    - "type" -- the Cartan type of a Coxeter group W
-    - "s" -- a number i, representing the simple reflection s=s_i
+    - "type" -- the Cartan type of a Coxeter group 'W'
+    - "s" -- a number 'i', representing the simple reflection 's=s_i'
     - "w" -- a tuple, considered as an expression(not necessarily reduced) for
-             an element of W.    
+             an element of 'W'.    
 
     OUTPUT:
-    - the product c_s*c_w of the Kazhdan Lusztig basis elements c_s and c_w in
-      the Hecke algebra of W, implemented as a dictionary, where the keys are
-      tuples encoding the basis elements, and the keys are their coefficients
-      in ZZ[v,v^(-1)].
+    - the product 'c_s*c_w' of the kl basis elements in the Hecke algebra of W,
+      implemented as a dictionary, where the keys are elements in 'W' indexing
+      the kl basis elements, and the values are their coefficients in
+      '\Z[v,v^(-1)]'.
+
+    EXAMPLES:
+
+        sage: s_times_w(['A',4],1,(1,2))
+        sage: {(1,2): v + 1/v}
+        
+        sage: s_times_w(['A',4],1,(2,3,1,4))
+        sage: {(1,2,1,3,4):1, (1,3,4): 1}
     
-    ALGORITHM:
-    Recall that if sw<w in the Bruhat order, then 
-        c_s * c_w = (v+v^(-1)) c_w;
-    otherwise, 
-        c_s * c_w = c_{sw} + \sum_{z:sz<z<w} \mu_{z,w} * c_z.
+    ALGORITHM: 
+    
+    Recall that if 'sw<w', then 'c_s * c_w = (v+v^(-1)) c_w'; 
+    otherwise, 'c_s * c_w = c_{sw} + \sum_{y:sy<y<w} \mu_{y,w} c_y'.
 
     .. NOTE: 
-        - the elements returned in this function are in their canonical reduced form; in
-          particular, the expression sw may not be one of the keys in the
-          dictionary.
+        - the elements returned in this function are in their canonical reduced
+          form; in particular, the expression 'w' or 'sw' may not be one of the
+          keys in the dictionary.
 
         - We are intentionally implementing expressions in W as tuples instead
           of lists here, in accordance with the use of dictionaries to encode
           linear combinations, for the keys in a dictionary can be tuples but
-          not list.  Note, however, that the Sage functions such as
-          W.bruhat_interval() all use expressions implemented as lists.  
+          not lists.  Note, however, that the Sage functions such as
+          W.bruhat_interval() all use expressions implemented as lists, so we
+          frequently have to convert tuples to lists or vice versa.  
     """
     
     W = CoxeterGroup(type, implementation = 'coxeter3') 
     d = defaultdict()
-
-    w_reduced = W(list(w)).reduced_word()   # a reduced word for w, as a list 
+    
+    # The canonical reduced word for w, produced by Sage as a list: 
+    w_reduced = W(list(w)).reduced_word()  
     
     if W(w_reduced).has_left_descent(s): 
         d[tuple(w_reduced)] = v + v**(-1)
     else:
         # Use the following three lines instead of d[(s,)+tuple(w_reduced)] = 1
         # to ensure that all tuples come from the normal form of elements
-        t = (s,) + tuple(w_reduced)
+        t = (s,) + w
         sw = W(list(t)).reduced_word()
         d[tuple(sw)] = 1 
         l = W.bruhat_interval([],w_reduced)
@@ -162,8 +142,11 @@ def s_times_w(type,s,w):
     return d
 
 
-
 def remove_zero(d):
+    """ 
+    Remove keys with value 0 from a dict/remove terms with coeff. 0 from a sum.
+    """
+
     dd = defaultdict(int)
     for k in d:
         if d[k] != 0:
@@ -171,18 +154,35 @@ def remove_zero(d):
     return dd
 
 
-# computation of c_w * c_v. 
 def sproduct_times_w(type,t,w):
-    """ Multiply a product of c_{s}'s with c_w.
+    r""" 
+    Multiply a product of 'c_{s}'s with 'c_w'.
 
     INPUT:
     - 'type' -- the Cartan type of the Coxeter group W
-    - 't' -- a tuple of simple reflections (s1,s2,...,sn)
+    - 't' -- a tuple of simple reflections '(s1,s2,...,sn)'
     - 'w' -- a tuple, representing an element of W
 
     OUTPUT:
     - the product (c_s1 * c_s2 * ... * c_sn) * c_w in the Hecke algebra of W.
     
+    EXAMPLES:
+
+        sage: sproduct_times_w(['A',4],(1,),(1,2))
+        sage: {(1,2): v + 1/v}
+
+        sage: sproduct_times_w(['A',4],(1,1),(1,2))
+        sage: {(1,2): (v + 1/v)^2}
+
+        sage: sproduct_times_w(['A',4],(2,1),(2,3,1,4))
+        sage: {(1,2,1,3,4): v + 1/v, (2,1,3,4): 1}
+        # compare with
+        sage: s_times_w(['A',4],1,(2,3,1,4))
+        sage: {(1,2,1,3,4): 1, (1,3,4): 1}
+        sage: s_times_w(['A',4],2,(1,2,1,3,4))
+        sage: {(1,2,1,3,4): v + 1/v}
+        sage: s_times_w(['A',4],2,(1,3,4))
+        sage: {(2,1,3,4): 1}
     """
 
     d = defaultdict()
@@ -197,10 +197,33 @@ def sproduct_times_w(type,t,w):
     return d
 
 def sproducts_times_w(type,d,w):
+    r"""
+    Compute the product of a sum and 'c_w', where the sum is a linear
+    combination of products of 'c_s's.
+
+    INPUT: 
+    - 'type' -- the Cartan type of the Coxeter group W
+    - 'd' -- a dictionary representing a linear combinations of products of
+            'c_s's, so the keys are like the input 't' in sproduct_times_w
+    - 'w' -- a tuple, representing an element of 'W'
+
+    EXAMPLE:
+
+        sage: sproducts_times_w(['A',4],{(2,):1,(1,3):v},(2,3,1,4))
+        sage: {(1,2,3,2,1,4): v, (2,1,3,4): v + 1/v, (1,3,4): (v+1/v)*v}
+        
+        # compare the above with the following
+
+        sage: sproduct_times_w(['A',4],(2,),(2,3,1,4))
+        sage: {(2,1,3,4): v + 1/v}
+        sage: sproduct_times_w(['A',4],(1,3),(2,3,1,4))
+        sage: {(1,2,3,2,1,4): 1, (1,3,4): v + 1/v}
+
+    """
+
     result = defaultdict()
     for k in d:
-        sproduct = tuple(itertools.chain.from_iterable(k))  # flatten d
-        dd = sproduct_times_w(type,sproduct,w)
+        dd = sproduct_times_w(type,k,w)
         for term in dd:
             if term in result:
                 result[term] += dd[term] * d[k]
@@ -211,16 +234,46 @@ def sproducts_times_w(type,d,w):
 
 
 def break_elt_once(type,w):
-    """
+    r"""
+    First step for breaking 'c_w' into linear comb. of products of 'c_s's.
+
     INPUT:
-    - 'w': an element of the Coxeter group, implemented as a tuple.
+    - 'type' -- the Cartan type of a Coxeter group 'W'
+    - 'w' -- a tuple, representing an element of 'W' 
 
     OUTPUT:
-    - a dictionary whose keys are tuples(products) of tuples(KL-basis
+    - a dictionary whose keys are tuples(products) of tuples(kl basis
       elements), and whose values are the coefficients of the products in
-      the expression for c_w.
-      Note: all but one key in the resulting dictionary are just a plain
-      KL-basis elements, considered as a "product of one element". 
+      the expression for c_w. See the following note for more.
+
+
+    .. NOTE:
+      - Suppose 'w' is a canonical reduced expression starting with 's', and
+        let 'w1' be the subword of 'w' following the 's'. Recall that
+
+        'c_s * c_{w1} = c_w + \sum_{y: sy<y<w1} \mu_{y,w1} c_y',
+
+        therefore
+
+        'c_w = c_s * c_{w1} - \sum_{y: sy<y<w1} \mu_{y,w1} c_y'.
+
+        The dictionary break_elt_once(type,w) returns is the following union:
+
+        '{((s,),(w1)):1} \cup \cup_{y:sy<y<w1} {(y,):-\mu_{y,w1}}'
+
+    EXAMPLES:
+
+        sage: break_elt_once(['A',4],(1,))
+        sage: {((1,),): 1}
+
+        sage: break_elt_once(['A',4],(1,2))
+        sage: {((1,),(2,)): 1}
+
+        sage: break_elt_once(['A',4],(1,2,1))
+        sage: {((1,),): -1, ((1,),(2,1)): 1}
+
+        sage: break_elt_once(['A',4],(1,2,3))
+        sage: {((1,),(2,3)): 1}
     """
 
     d = defaultdict()
@@ -240,27 +293,29 @@ def break_elt_once(type,w):
         d[((s,),w1)] = 1
 
     return d
-
-def done(t):
-    """ Determine if a tuple represents a product of c_s's.
-
-    INPUT:
-    - t: a tuple of tuples, encoding the product of the KL-basis elements whose
-      indices are its entries(which are tuples themselves).
-
-    OUTPUT: 
-    - a boolean value, describing if all the basis elements in the product are 
-      already simple relfections
-    """
     return len(t[-1]) == 1
 
 
-def break_product(type,t):
-    """ Break further a product c_s*...*c_w where w is not a simple reflection.
+def break_further(type,t):
+    r""" 
+    Break further a product 'c_s*...*c_w' if 'w' is not a simple reflection.
+
     INPUT:
     - 't': a tuple(product) of tuples(elements), of the form 
-           t = ((s1,),...,(sn,),(w)),
-           where w is not a simple reflection, so that t needs breaking.
+           't = ((s1,),...,(sn,),(w))'
+           where 'w' is not a simple reflection.
+
+    OUTPUT:
+    - a dictionary obtained from the dictionary break_elt_once(type,w) by
+      prepending the tuple ((s1,),...,(sn)) to all the keys.
+
+    EXAMPLES:
+
+        sage: break_further(['A',4],((4,),(3,),(1,2,1)))
+        sage: {((4,),(3,),(1,))): -1, ((4,),(3,),(1,),(2,1)): 1}
+        # compare with
+        sage: break_elt_once(['A',4],(1,2,1))
+        sage: {((1,),): 1, ((1,),(2,1)): 1}
 
     """
     result = defaultdict()
@@ -272,10 +327,64 @@ def break_product(type,t):
 
 
 
+def done(t):
+    r""" 
+    Determine if a tuple represents a product of c_s's.
+
+    INPUT:
+    - t: a tuple of tuples, encoding the product of the KL-basis elements whose
+      indices are its entries(which are tuples themselves).
+
+    OUTPUT: 
+    - a boolean value, describing if all the basis elements in the product are 
+      already simple relfections
+    
+    EXAMPLES:
+    
+        sage: done(((1,),))
+        sage: True
+
+        sage: done(((1,),(2,)))
+        sage: True
+
+        sage: done(((1,),(2,1)))
+        sage: False
+
+    """
+
+    return len(t[-1]) == 1
+
+
 def break_elt(type,w):
+    r"""
+    Write 'c_w' as a linear combination of product of 'c_s's.
+    
+    INPUT:
+    - 'type' -- the Cartan type of a Coxeter group 'W'
+    - 'w' -- a tuple, representing an element of 'W' 
+    
+    OUTPUT:
+    - a dictionary encoding how to write 'c_w' as a linear combination of
+      products of 'c_s's 
+      
+    EXAMPLE:
+
+        # For type ['A',4], 'c_{121} = c_1c_{21} - c_1 = c_1c_2c_1 - c_1', so
+     
+        sage: break_elt(['A',4], (1,2,1))
+        sage: {(1,): -1, (1,2,1):1}
+
+        sage: break_elt(['A',4], (2,1,3,2,1,4))
+        sage: {(2,1,3,2,1,4): 1, (2,1,3,4): -1, (1,4): 1, (1,2,1,4): -1}
+        
+        # The above result shows 'c_{213214}' can be written as follows:
+            'c_2c_1c_3c_2c_1c_4 - c_2c_1c_3c_4 + c_1c_4 + c_1c_2c_1c_4'
+    """
     result = defaultdict()
+
     to_be_checked = defaultdict()
     to_be_checked[(w,)] = 1
+
     while bool(to_be_checked): #equivalent to 'while to_be_checked is nonempty'
         new_to_be_checked = defaultdict()
         for t in to_be_checked:
@@ -285,20 +394,68 @@ def break_elt(type,w):
                 else:
                     result[t] = to_be_checked[t]
             else:
-                dd = break_product(type,t)
+                dd = break_further(type,t)
                 for k in dd:
                     if k in new_to_be_checked:
                         new_to_be_checked[k] += dd[k] * to_be_checked[t]
                     else:
                         new_to_be_checked[k] = dd[k] * to_be_checked[t]
         to_be_checked = new_to_be_checked
-    return result
-                
-def v_times_w(type,v,w):
-    """ Compute c_v*c_w in the Hecke algebra.
+
+    flatten_result = defaultdict()
+    for k in result:
+        sproduct = tuple(itertools.chain.from_iterable(k))  # flatten d
+        flatten_result[sproduct] = result[k]
+    return flatten_result
+               
+
+
+
+
+
+
+
+
+
+########################### Code for future use ###############################
+
+var('v')
+
+def kl(type,y,w):
+    """ Compute a Kazhdan-Lusztig polynomial 'p_{y,w}'.
+
+    INPUT:
+    - "type" -- the Cartan type of a Coxeter group 'W'
+    - "y", "w" -- tuples, considered as expressions(not necessarily reduced) for
+                 elements in 'W'
+   
+    OUTPUT:
+    - the Kazhdan-Lusztig polynomial 'p_{y,w}', returned in our convention. 
+
+
+    .. TODO::
+    - use Coxeter matrix instead of Cartan type to specify Coxeter groups.
+    """
+    
+    # compute the kl-polynomial (in 'q') using Coxeter3
+    W = CoxeterGroup(type,implementation='coxeter3')
+    f = W.kazhdan_lusztig_polynomial(y,w) 
+    # convert the Coxeter3 result using our normalization ('v' instead of 'q')
+    g = f.substitute(q=v**2)*v**(len(y)-len(w))
+    return g.expand()
+
+def mu(type,y,w):
+    """ Compute the mu_{y,w}, the mu-coefficient for a pair of elements y,w.
+    
+    .. TODO::
+        Use the coxeter3 package to directly compute mu_{y,w} instead of 
+        reading it off as a coefficient of p_{y,w}.
+        The direct computation can be---and is---done in s_times_w, so this
+        function is actually not used.
+
+    .. NOTE::
+        It is known that '\mu_{y,w}' equals the coefficient of 'v^{-1}' in the
+        kl-polynomials 'p_{y,w}'
     """
 
-    d = break_elt(type,v)
-    return sproducts_times_w(type,d,w)
-
-
+    return kl(type,y,w).coefficient(v,-1)
