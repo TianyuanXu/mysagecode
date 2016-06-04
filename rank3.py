caad4c17cdb5c524a3a94dfca9a6e9f972a1af8c
Tianyuan Xu <tianyuan@uoregon.edu>
2016-06-04 15:50:47 -0700

    make multiplication table

--- rank3.py
+++ rank3.py
@@ -1,4 +1,30 @@
+import pprint
+import itertools
 
+
+def f(b):
+    return 2 * b + 3
+
+def all_length(n):
+    l = list(itertools.product([0,1],repeat=n))
+    ll = [int(''.join(map(str,map(f,i)))) for i in l]
+    return ll 
+        
+def all_less(n):
+    l = [all_length(i) for i in range(1,n)]
+    return reduce(lambda x,y: x+y, l)
+
+def table(rows,cols,b):
+    ll = [[str(i)]+[path_prod(i,j) for j in cols] for i in rows]
+    top_row = ['0'] + [str(i) for i in cols]
+    lll = [top_row] + ll
+    for i in lll:
+        i = [j.rjust(b) for j in i]
+        print(''.join(i))
+
+# for i in list_of_lists:
+    # i = [str(j).rjust(2n) for j in i]
+    # print(''.join(i))
 
 def triangle(m12,m13,m23):
     m = matrix(3,[1,m12,m13,m12,1,m23,m13,m23,1])
@@ -16,9 +42,8 @@ def code_to_word(s):
 
 def path_prod(s,t):
     word_dic = t_basis_product(code_to_word(s),code_to_word(t),path(4,6))
-    for key in word_dic:
-        print word_to_code(key), '*' , word_dic[key]
-    
+    l = [str(word_to_code(key))+'*'+str(word_dic[key]) for key in word_dic]
+    return '+'.join(l)
 
 def word_to_code(t):
     x = dihedral_segments(t)
