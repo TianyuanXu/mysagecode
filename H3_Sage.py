e7e2436b50484f978504bf3dd3c22f4b155f5c01
Tianyuan Xu <tianyuan@uoregon.edu>
2017-03-13 10:35:48 -0700

    added cell data for H3 and H4

---
+++ H3_Sage.py
@@ -0,0 +1,38 @@
+h3=CoxeterGroup(['H',3])
+
+"""
+Coxeter Matrix for H4 in Coxeter 3 is
+
+[1 5 2] 
+[5 1 3] 
+[2 3 1] 
+
+while the matrix for Sage is
+
+[1 3 2]
+[3 1 5]
+[2 5 1].
+"""
+
+
+"""
+The set of all elements that are fully-commutative but not subregular form one
+single 2-sided cell. The following has been converted to the Sage convention. 
+"""
+
+fcsage=[31,312,231,3231,3123,2312,32312,31232,23231,23123,323123,312321,232312,231232,123231,3231232,2323123,2312321,1232312,32312321,23231232,12323123,232312321,123231232,1232312321]
+
+
+
+
+"""
+It splits into 5 left cells. 
+"""
+l1sage = [31,231,3231,23231,123231]
+l2sage = [312,2312,32312,232312,1232312]
+l3sage = [3123,23123,323123,2323123,12323123]
+l4sage = [32323,312323,2312323,32312323,232312323]
+l5sage = [31232,231232,3231232,23231232,123231232]
+
+
+
